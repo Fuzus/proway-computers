e36f9896c4686e17e65cb05c9b6910fac1dde91e
Fuzus <matheusdamata2000@gmail.com>
2023-02-26 22:46:29 -0300

    criando funcionalidade da barra de pesquisa

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduto } from '../produtos';
 import { ProdutosService } from '../produtos.service';
 
@@ -10,10 +11,25 @@ import { ProdutosService } from '../produtos.service';
 export class ProdutosComponent implements OnInit{
   produtos: IProduto[] | undefined;
 
-  constructor(private produtoService : ProdutosService){}
+  constructor(
+    private produtoService : ProdutosService,
+    private route: ActivatedRoute
+    ){}
 
   ngOnInit(): void {
-      this.produtos = this.produtoService.getAll();
+    const produtos = this.produtoService.getAll();
+    
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+
+      if(descricao) {
+        this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+        return;
+      }
+
+      this.produtos = produtos;
+    });  
   }
+
 
 }
