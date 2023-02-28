0970b21543fc13c6a688be7090641fdd6d9eec2f
Fuzus <matheusdamata2000@gmail.com>
2023-02-28 15:42:40 -0300

    Iniciando criação do backend da aplicação

---
+++ backend/proway-computers-api/src/main/java/br/com/fuzus/prowaycomputersapi/ProwayComputersApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fuzus.prowaycomputersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProwayComputersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProwayComputersApiApplication.class, args);
+	}
+
+}
---
+++ backend/proway-computers-api/src/test/java/br/com/fuzus/prowaycomputersapi/ProwayComputersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fuzus.prowaycomputersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProwayComputersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
